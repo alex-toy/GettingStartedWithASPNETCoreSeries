b05e13080995365c74daeb2b5ba81b6daea31fab
alex-toy <alexei.80@hotmail.fr>
2023-07-09 10:03:23 +0200

    parameters

---
+++ MiddlewareApp/ModelBindingApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ModelBindingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [BindProperties(SupportsGet = true)]
+    public class WeatherForecastController : ControllerBase
+    {
+        //[BindProperty(SupportsGet=true)]
+        public bool IsSet { get; set; }
+
+        [BindProperty(SupportsGet = true, Name="test")]
+        public bool IsTestProp { get; set; }
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id, bool isTest)
+        {
+            return $"id : {id} - isTest: {isTest} - IsSet: {IsSet} - IsTestProp: {IsTestProp}";
+        }
+
+        [HttpGet("test/{id}")]
+        public string Get([FromQuery] int id, [FromHeader(Name = "Accept-Language")] string language)
+        {
+            return $"id : {id} - language : {language}";
+        }
+
+        [HttpGet("complex")]
+        public string Get([FromQuery]MyComplexType complexType)
+        {
+            string parameters = $"Id : {complexType.Id} - Language : {complexType.Language} - IsBool : {complexType.IsBool}";
+
+            if (complexType.Marks == null) return parameters;
+
+            foreach (var mark in complexType.Marks)
+            {
+                parameters += $" mark {mark.ToString()}";
+            }
+
+            return parameters;
+        }
+    }
+}
---
+++ MiddlewareApp/ModelBindingApp/MyComplexType.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingApp
+{
+    public class MyComplexType
+    {
+        [FromQuery]
+        public int Id { get; set; }
+
+        [FromHeader(Name = "Accept-Language")]
+        public string Language { get; set; }
+
+        [BindProperty(SupportsGet = true, Name = "isBool")]
+        public bool IsBool { get; set; }
+
+        public int[] Marks { get; set; }
+    }
+}
